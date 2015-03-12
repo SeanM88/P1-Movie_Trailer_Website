3abc20390be2c34e3a48665084a3287326a058b0
Sean Megan <sean.megan88@gmail.com>
2015-03-11 20:23:43 -0400

    Initial commit of Movie Trailer Website project

---
+++ entertainment_center.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import media
+import fresh_tomatoes
+
+pacific_rim = media.Movie("Pacific Rim",
+                        "Robots fight aliens",
+                        "http://content7.flixster.com/movie/11/17/39/11173933_800.jpg",
+                        "http://www.youtube.com/watch?v=2vKz7WnU83E")
+
+the_dark_knight = media.Movie("The Dark Knight",
+                     "The sequel to Batman Begins.",
+                     "http://ia.media-imdb.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX640_SY720_.jpg",
+                     "http://www.youtube.com/watch?v=GROmJWb-3wU")
+
+fight_club = media.Movie("Fight Club",
+                         "A movie about more than fighting",
+                         "http://images.moviepostershop.com/fight-club-movie-poster-1999-1020215604.jpg",
+                         "http://www.youtube.com/watch?v=SUXWAEX2jlg")
+
+movies = [pacific_rim, the_dark_knight, fight_club]
+
+fresh_tomatoes.open_movies_page(movies) 
---
+++ media.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import webbrowser
+
+class Movie():
+    
+    valid_ratings["G","PG","PG-13","R"]
+    
+    def __init__(self, movie_title, movie_plot, poster_image, youtube_trailer):
+        self.title = movie_title
+        self.plot = movie_plot
+        self.poster_image_url = poster_image
+        self.youtube_trailer_url = youtube_trailer
+        
+    def show_trailer(self):
+        webbrowser.open(self.youtube_trailer_url)
+        
